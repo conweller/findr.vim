dde3baa80818883282b19e865fe75ebb9caf7238
Connor Onweller <onweller@udel.edu>
2020-01-03 23:20:56 -0500

    Todo list, fixed <c-h> mapping

--- lua/findr/init.lua
+++ lua/findr/init.lua
@@ -1,3 +1,13 @@
+-- TODO:
+--  - history
+--  - floating window border
+--  - variables:
+--      - border
+--      - g:findr_enable_border
+--      - g:findr_floating_window
+--      - g:findr_border
+--      - g:findr_max_hist
+
 local M = {}
 M.controller = require('findr.controller')
 M.start_loc = 1
