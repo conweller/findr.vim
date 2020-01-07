590b275256708f1b925539ee5783da6acdb39737
Connor Onweller <onweller@udel.edu>
2020-01-07 12:13:34 -0500

    Test buffers

--- lua/findr/sources/buffers.lua
+++ lua/findr/sources/buffers.lua
@@ -2,13 +2,14 @@ local M = {}
 local vim = vim
 
 local function buf_valid(buf)
-    if buf == vim.api.nvim_call_function('bufnr',{}) then
-        return false
-    elseif vim.api.nvim_call_function('buflisted', {buf}) == 1 then
-        local name = vim.api.nvim_buf_get_name(buf)
-        return name ~= ''
-    end
-    return false
+    return true
+    -- if buf == vim.api.nvim_call_function('bufnr',{}) then
+    --     return false
+    -- elseif vim.api.nvim_call_function('buflisted', {buf}) == 1 then
+    --     local name = vim.api.nvim_buf_get_name(buf)
+    --     return name ~= ''
+    -- end
+    -- return false
 end
 
 local function display(line)
